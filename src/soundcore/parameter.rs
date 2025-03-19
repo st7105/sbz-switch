845ff1f177c84db90fe43cd5de1468e537b1e4ed
st7105 <st7105@gmail.com>
2025-03-20 00:43:05 +0300

    add Display for SoundCoreParamValue

--- src/soundcore/parameter.rs
+++ src/soundcore/parameter.rs
@@ -1,5 +1,5 @@
 use std::mem::MaybeUninit;
-use std::str;
+use std::{fmt, str};
 use serde_derive::{Deserialize, Serialize};
 use tracing::{info, trace, trace_span};
 use windows::Win32::Foundation::E_ACCESSDENIED;
@@ -23,18 +23,29 @@ pub enum SoundCoreParamValue {
     None,
 }
 
+impl fmt::Display for SoundCoreParamValue {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            SoundCoreParamValue::Float(value) => write!(f, "{}", value),
+            SoundCoreParamValue::Bool(value) => write!(f, "{}", value),
+            SoundCoreParamValue::U32(value) => write!(f, "{}", value),
+            SoundCoreParamValue::I32(value) => write!(f, "{}", value),
+            SoundCoreParamValue::None => write!(f, "-"),
+        }
+    }
+}
+
 impl PartialEq for SoundCoreParamValue {
     fn eq(&self, other: &Self) -> bool {
         match (self, other) {
             (SoundCoreParamValue::Float(a), SoundCoreParamValue::Float(b)) => {
-                // Сравнение f32: используем точное равенство или можно добавить epsilon
-                a == b || (a.is_nan() && b.is_nan()) // Учитываем NaN как равные
+                a == b || (a.is_nan() && b.is_nan())
             }
             (SoundCoreParamValue::Bool(a), SoundCoreParamValue::Bool(b)) => a == b,
             (SoundCoreParamValue::U32(a), SoundCoreParamValue::U32(b)) => a == b,
             (SoundCoreParamValue::I32(a), SoundCoreParamValue::I32(b)) => a == b,
             (SoundCoreParamValue::None, SoundCoreParamValue::None) => true,
-            _ => false, // Разные варианты не равны
+            _ => false,
         }
     }
 }
